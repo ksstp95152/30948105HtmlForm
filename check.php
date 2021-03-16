730bb84b15a00d00dddae8cd5e7d0e2d98557312
ksstp95152 <76685616+ksstp95152@users.noreply.github.com>
2021-03-17 00:15:31 +0800

    Add files via upload

--- check.php
+++ check.php
@@ -5,14 +5,14 @@
     //echo $_POST["msg"]."<br>";
 
     
-    // if($_POST["e-mail"]=="root" && $_POST["password"] == "123")
-    //   {
-    //       header("Location:https://www.acgist.com/demo/jigoku/index.html");
-    //   }
-    //   else
-    //   {
-    //       header("Location:https://www.acgist.com/demo/jigoku/index.html");
-    //   }
+    if($_POST["e-mail"]=="root" && $_POST["password"] == "123")
+      {
+          header("Location:https://www.acgist.com/demo/jigoku/index.html");
+      }
+      else
+      {
+          header("Location:https://www.acgist.com/demo/jigoku/index.html");
+      }
 
 
     // echo $_POST["e-mail"]."<br>";
